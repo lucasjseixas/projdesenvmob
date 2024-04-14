a4c0009fb695b84076a31f359864b9b5b9df0dcd
Lucas <lucasjoseseixas@alunos.utfpr.edu.br>
2024-04-14 15:01:25 -0300

    adicionado dropdownmenu basico e alguns campos de cadastro do candidato

--- lib/Pages/LoginPages/Forms/cadidato_form.dart
+++ lib/Pages/LoginPages/Forms/cadidato_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projec1/utils/dropdownmenu.dart';
 
 class CandidatoForm extends StatefulWidget {
   const CandidatoForm({super.key});
@@ -8,12 +9,68 @@ class CandidatoForm extends StatefulWidget {
 }
 
 class _CandidatoFormState extends State<CandidatoForm> {
+  final TextEditingController _dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulario de cadastro de Candidatos'),
       ),
+      body: Column(
+        children: [
+          TextFormField(
+            key: const Key('nomecandidato'),
+            decoration: const InputDecoration(labelText: 'Nome Completo'),
+            keyboardType: TextInputType.text,
+          ),
+          TextFormField(
+            key: const Key('enderecocandidato'),
+            decoration: const InputDecoration(labelText: 'Endereco Completo'),
+            keyboardType: TextInputType.streetAddress,
+          ),
+          TextFormField(
+            controller: _dateController,
+            key: const Key('datanascimentocandidato'),
+            decoration: const InputDecoration(
+              labelText: 'Data de Nascimento',
+              prefixIcon: Icon(Icons.calendar_today),
+            ),
+            readOnly: true,
+            onTap: () {
+              _selectDate();
+            },
+          ),
+          TextFormField(
+            key: const Key('emailcandidato'),
+            decoration: const InputDecoration(labelText: 'E-mail'),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          TextFormField(
+            key: const Key('confirmaremailcandidato'),
+            decoration: const InputDecoration(labelText: 'Confirme o E-Mail'),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          const Text('Estado'),
+          const DropDownMenu(),
+        ],
+      ),
     );
+  }
+
+  Future<void> _selectDate() async {
+    DateTime? _picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2006),
+      lastDate: DateTime(2100),
+    );
+    if (_picked != null) {
+      setState(
+        () {
+          _dateController.text = _picked.toString().split(" ")[0];
+        },
+      );
+    }
   }
 }
---
+++ lib/utils/dropdownmenu.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+List<String> siglaestados = <String>[
+  'AC',
+  'AL',
+  'AP',
+  'AM',
+  'BA',
+  'CE',
+  'ES',
+  'GO',
+  'MA',
+  'MS',
+  'MT',
+  'MG',
+  'PA',
+  'PB',
+  'PR',
+  'PE',
+  'PI',
+  'RJ',
+  'RN',
+  'RS',
+  'RO',
+  'RR',
+  'SC',
+  'SP',
+  'SE',
+  'TO'
+];
+
+class DropDownMenu extends StatefulWidget {
+  const DropDownMenu({super.key});
+
+  @override
+  State<DropDownMenu> createState() => _DropDownMenuState();
+}
+
+class _DropDownMenuState extends State<DropDownMenu> {
+  String dropdownValue = siglaestados.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<String>(
+      initialSelection: siglaestados.first,
+      onSelected: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      dropdownMenuEntries:
+          siglaestados.map<DropdownMenuEntry<String>>((String value) {
+        return DropdownMenuEntry<String>(value: value, label: value);
+      }).toList(),
+    );
+  }
+}
